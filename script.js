d5e1a6d60b249b0dc2055f06bd9ba0aabd738976
luminousherbs <172189078+luminousherbs@users.noreply.github.com>
2025-08-08 12:37:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-alert("hi")
+// alert("hi")
+const locations = [
+  "London",
+  "Paris",
+  "Birmingham",
+  "Slough",
+  "the moon",
+  "Coventry",
+  "Manchester",
+]
 function roll() {
-  alert("roll")
-  place.textContent = "Paris";
+  // alert("roll")
+  place.textContent = locations[Math.floor(Math.random() * locations.length)];
 }
